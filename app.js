66e08aa8b623f171269c8ee0d5c637ecd95be0d9
audywp <audy.wisudap@gmail.com>
2020-05-26 10:57:48 +0700

    create app

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: false })) //parsing data from urlencoded
+app.use(bodyParser.json()) // parsing data from JSON
+
+const port = process.env.PORT
+app.listen(port, function () {
+  console.log(`Listening port ${port}`)
+})
